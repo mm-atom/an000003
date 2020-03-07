06140493c64a8f8715ae4edc02e6b8ab2593cad7
taoqf <tao_qf@163.com>
2020-03-07 12:51:08 +0800

    ai

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export default function fun() {
+import { IAi, ICommonParams, IHeaders } from '@mmstudio/an000002';
+
+export default interface IAiNodejsPage extends IAi {
+	readonly data: {
+		readonly [attr: string]: unknown;
+		readonly msg: ICommonParams;
+		readonly headers: IHeaders;
+		readonly actionid: string;
+		readonly params: { [key: string]: unknown };
+		readonly url: string;
+	};
+	// eslint-disable-next-line semi
 }
