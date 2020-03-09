ceedd1a62e382f3acd8d287a9e7638955052c94c
taoqf <tao_qf@163.com>
2020-03-09 11:18:57 +0800

    type params

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export default interface IAiNodejsPage extends IAi {
 		readonly msg: ICommonParams;
 		readonly headers: IHeaders;
 		readonly actionid: string;
-		readonly params: { [key: string]: unknown };
+		readonly params: { [key: string]: string };
 		readonly url: string;
 	};
 	// eslint-disable-next-line semi
